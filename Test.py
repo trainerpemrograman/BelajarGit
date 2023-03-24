c02ed8cc0fefe84fbdfdf3ed36de7e0a077d81ed
trainerpemrograman <trainer_pemrograman@gamelab.id>
2023-03-24 14:20:26 +0700

    ini adalah perubahan fitur A karena lupa

--- Test.py
+++ Test.py
@@ -1,3 +1,3 @@
-print("Coba testing git dengan code python terjadi perubahan pada fitur A")
+print("Coba testing git dengan code python terjadi perubahan pada fitur A adalah milik A")
 print("Coba melakukan fitur A")
 
